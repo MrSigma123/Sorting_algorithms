203c5050861b8fe9ad8d63afa869cb533a0cbdcc
MrSigma123 <karol.czolpinski@gmail.com>
2025-02-08 23:55:20 +0100

    Declare insertion sort function

--- insertion_sort/insertion_sort.c
+++ insertion_sort/insertion_sort.c
@@ -1,9 +1,17 @@
 // first attempt to write insertion sort
 #include <stdio.h>
 
+void insertion_sort(int * source_arr, int * dest_arr, int size);
+
 int main(void)
 {
   // TODO: prepare input data and the algorithm core
 
   return 0;
 }
+
+void insertion_sort(int * source_arr, int * dest_arr, int size)
+{
+
+}
+
