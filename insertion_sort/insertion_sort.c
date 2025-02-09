45125cf659e77606f1ebe56d7a530ab546ce67ea
MrSigma123 <karol.czolpinski@gmail.com>
2025-02-09 23:47:57 +0100

    Add input data (store source array)

--- insertion_sort/insertion_sort.c
+++ insertion_sort/insertion_sort.c
@@ -1,11 +1,23 @@
 // first attempt to write insertion sort
 #include <stdio.h>
+#include <stdlib.h>
 
 void insertion_sort(int * source_arr, int * dest_arr, int size);
 
 int main(void)
 {
-  // TODO: prepare input data and the algorithm core
+  int size;
+  int i;
+  printf("Enter the size of array: ");
+  scanf("%d", &size);
+  int * source = (int*)malloc(sizeof(int) * size);
+  int * dest = (int*)malloc(sizeof(int) * size);
+
+  // scan the input array
+  for (i = 0; i < size; i++)
+  {
+    scanf("%d", &source[i]);
+  }
 
   return 0;
 }
